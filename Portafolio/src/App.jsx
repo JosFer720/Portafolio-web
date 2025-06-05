bd99ee06eaafce7634476144e2b2d35f10a3b9ff
JosFer720 <119770216+JosFer720@users.noreply.github.com>
2025-06-04 19:26:14 -0600

    config

--- Portafolio/src/App.jsx
+++ Portafolio/src/App.jsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Portfolio from './pages/Portfolio';
 import AboutMe from './pages/AboutMe';
 import Projects from './pages/projects';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Portfolio />} />
-        <Route path="/about" element={<AboutMe />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Portfolio />} />
+          <Route path="/about" element={<AboutMe />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
